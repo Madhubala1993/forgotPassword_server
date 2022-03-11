651382fedde82c78366d1423b6b2d0a9b0fe0c00
Madhubala1993 <madhu.a1993@gmail.com>
2022-03-11 21:12:59 +0530

    mail

--- user.js
+++ user.js
@@ -70,8 +70,7 @@ async function sendMail(mailid, otp_number, req, response) {
 
   mailTransporter.sendMail(mailDetails, async (err) => {
     if (err) {
-      return console.log(process.env.mail_id, process.env.password);
-      // response.status(400).send("email is not sent");
+      return response.status(400).send("email is not sent");
     }
     return response.send({
       message: "OTP sent to your e-mail",
